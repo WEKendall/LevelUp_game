23316388473ea92525daf2c40e7148373ec9622f
WEKendall <kendall.william.e@gmail.com>
2023-09-22 14:28:00 +0000

    gamestatus clean code

--- src/levelup/gamestatus.py
+++ src/levelup/gamestatus.py
@@ -8,7 +8,6 @@ class GameStatus():
     current_position: Position = (0,0)
     move_count: int = 42
 
-def __init__(self):
-    self.character_name = "Bob the Builder"
-    self.current_position = tuple ((3,4))
-    self.move_count = 42
+
+
+
